f068b85c3cafc12bf48e333f73915f8f5f133d13
Antônio Neto <39776733+joseantonioneto@users.noreply.github.com>
2024-02-02 19:31:13 -0300

    Adicionanando arquivos
    Adicionando arquivos iniciais do proejto

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        projetos Projeto1 = new projetos("projeto1","rua joao 1","joao","Escopo1","21/06/2024");
+        Projeto1.displayInfo();
+    }
+}
---
+++ projetos.java
@@ -0,0 +1,24 @@
+
+public class projetos {
+    String NomeProjeto;
+    String localizacao;
+    String Cliente;
+    String Escopo;
+    String DataInicial;
+
+    public projetos(String nomeProjeto, String localizacao, String cliente, String escopo, String dataInicial) {
+        this.NomeProjeto = nomeProjeto;
+        this.localizacao = localizacao;
+        this.Cliente = cliente;
+        this.Escopo = escopo;
+        this.DataInicial = dataInicial;
+    }
+    public void displayInfo(){
+        System.out.println("Nome do projeto: "+ NomeProjeto);
+        System.out.println("Localização: "+ localizacao);
+        System.out.println("Nome do cliente: "+ Cliente);
+        System.out.println("Escopo do projeto: "+ Escopo);
+        System.out.println("Data incial do projeto: "+ DataInicial);
+    }
+    public void editarprojeto();
+}
